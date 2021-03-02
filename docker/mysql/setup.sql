a6d26647e0bf5e0adf3ad9e4f630434150fc0be6
Vitalik <petergerinv@gmail.com>
2021-03-02 22:02:25 +0200

    feat: rename folder to project.

--- docker/mysql/setup.sql
+++ docker/mysql/setup.sql
@@ -1,9 +1,9 @@
 -- create the databases
-CREATE DATABASE IF NOT EXISTS orthoshop;
+CREATE DATABASE IF NOT EXISTS laravel-beyond-crud;
 
 -- create the users for each database
 CREATE USER IF NOT EXISTS 'user'@'%';
 ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
-GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `orthoshop`.* TO 'user'@'%';
+GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `laravel-beyond-crud`.* TO 'user'@'%';
 
 FLUSH PRIVILEGES;
